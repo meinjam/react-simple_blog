11af8bc40cc1a56c90adb14668a840ae8228a02d
meinjam <injam.nd.jsr@gmail.com>
2021-01-25 20:31:56 +0600

    single blog and 404 page done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Create from "./components/Create";
+import BlogDetails from "./components/BlogDetails";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -10,11 +12,17 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
             <Route path="/create">
               <Create />
             </Route>
-            <Route path="/">
-              <Home />
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>TNN Blog</h1>
+      <Link to="/">
+        <h1>TNN Blog</h1>
+      </Link>
       <div className="links">
-        <Link to="/">Home</Link>
         <Link to="/create">New Blog</Link>
       </div>
     </nav>
